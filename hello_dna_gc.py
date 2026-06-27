200eaf8365f7eab13ecc03cd863eb93bbb0fc351
Genomicsnikku <n3016507@gmail.com>
2026-06-27 12:24:29 +0530

    Level-1 : Hello DNA with GC%
    This script calculates and displays the nucleotide composition and GC content of a DNA sequence.

---
+++ hello_dna_gc.py
@@ -0,0 +1,16 @@
+print("Hello DNA 🧬")
+
+dna = "ATGCGATATCGCG"
+dna = dna.upper()  # Chote-bade letter ka issue khatam
+
+a = dna.count('A')
+t = dna.count('T')
+g = dna.count('G')
+c = dna.count('C')
+total = len(dna)
+gc_percent = (g + c) / total * 100
+
+print("Sequence:", dna)
+print("Length:", total)
+print("A:", a, " T:", t, " G:", g, " C:", c)
+print(f"GC% = {gc_percent:.2f}%")
